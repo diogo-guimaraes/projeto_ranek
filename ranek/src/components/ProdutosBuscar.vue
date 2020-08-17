29737aa9a6f809e74893ebbf46692662d68b0100
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-17 00:30:42 -0400

    refactor(compoments): ProdutosLista
    * add form
    
    * add input id buscar passando a diretiva v-model passando o data que contem o return busca que inicia vazio
    
    * add methods-> buscarProdutos
    
    muda a url dificil parametros
    (key e value)

--- ranek/src/components/ProdutosBuscar.vue
+++ ranek/src/components/ProdutosBuscar.vue
@@ -4,11 +4,30 @@
     add(compoments): ProdutosLista
     rm(compoments): ProdutosLista
   -->
-  <div></div>
+  <form action="">
+    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
