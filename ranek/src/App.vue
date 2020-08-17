25423890c63968be7be433ce7a2fd82b0c5e11d6
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-16 20:58:57 -0400

    refactor(componets) App, main, store
    * adjust syntax

--- ranek/src/App.vue
+++ ranek/src/App.vue
@@ -7,6 +7,9 @@
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
 export default {
   components: {
     TheHeader,
