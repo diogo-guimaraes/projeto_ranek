3b6ea0b9df1dbc89b5892e520a60d445f5ba134c
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-16 23:15:24 -0400

    refactor(compoments): ProdutosLista
    * add class-> section="produtos-container"
    
    * , div with v-for fazendo um foreach no component produto, ( api ranek.json servida pelo json serve), definindo a key como id de dentro da api
    
    * add img pssando um v-for e v-if para verificar se existe foto ou não
    
    *

--- ranek/src/components/ProdutosLista.vue
+++ ranek/src/components/ProdutosLista.vue
@@ -4,9 +4,18 @@
     add(compoments): ProdutosLista
     rm(compoments): ProdutosLista
   -->
-  <div>
-    {{ produtos }}
-  </div>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
