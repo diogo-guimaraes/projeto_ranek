f5408d4d52a7136dc7f8fa5d8a0d8a0c1e3dc079
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-16 22:25:59 -0400

    add(compoments): ProdutosLista
    * import components -> ProdutosBuscar, ProdutosLista
    
    * add components -> ProdutosBuscar, ProdutosLista

---
+++ ranek/src/components/ProdutosLista.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 
+    refactor(compoments): ProdutosLista
+    add(compoments): ProdutosLista
+    rm(compoments): ProdutosLista
+  -->
+  <div></div>
+</template>
+
+<script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+export default {
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
+</script>
+
+<style scoped></style>
