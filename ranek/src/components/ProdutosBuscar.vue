e376e425a08b12f384c6606b651aecd3b4815c02
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-16 22:29:02 -0400

    add(compoments): ProdutosLista

---
+++ ranek/src/components/ProdutosBuscar.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- 
+    refactor(compoments): ProdutosLista
+    add(compoments): ProdutosLista
+    rm(compoments): ProdutosLista
+  -->
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
