7bcafd1c16a4376f92f5154e41273fb2ab01b7af
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-16 21:14:38 -0400

    refactor(compoments): TheHeader
    * adc style TheHeader

--- ranek/src/components/TheHeader.vue
+++ ranek/src/components/TheHeader.vue
@@ -1,10 +1,30 @@
 <template>
+  <!-- refactor(compoments): TheHeader
+add(compoments): TheHeader
+  rm(compoments): TheHeader-->
   <p>Header</p>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "TheHeader",
+};
 </script>
 
-<style>
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+
+.logo {
+  padding: 10px 0;
+}
+
+.logo img {
+  width: 90px;
+}
 </style>
