5891c638e38dd6c60db9cd10fbadcb5fcaa871e9
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-17 00:12:58 -0400

    refactor(compoments): ProdutosLista
    * add import api services.js
    
    importa o arquivo service para fazermos as chamadas na api
    
    * add novo padrão de requisição. -> axios

--- ranek/src/components/ProdutosLista.vue
+++ ranek/src/components/ProdutosLista.vue
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { api } from "@/services.js";
 export default {
   data() {
     return {
@@ -27,11 +28,9 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto")
-        .then((response) => response.json())
-        .then((response) => {
-          this.produtos = response;
-        });
+      api.get("/produto").then((response) => {
+        this.produtos = response.data;
+      });
     },
   },
   created() {
