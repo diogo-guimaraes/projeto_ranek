d45a36af9a3c9c8171326a7dd93f4cf039340c57
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-19 19:14:44 -0400

    refactor(compoments): ProdutosLista
    * somente ajustei o código

--- ranek/src/components/ProdutosBuscar.vue
+++ ranek/src/components/ProdutosBuscar.vue
@@ -4,14 +4,9 @@
     add(compoments): ProdutosLista
     rm(compoments): ProdutosLista
   -->
-  <form action="">
+  <form>
     <input name="busca" id="busca" type="text" v-model="busca" />
-    <input
-      type="submit"
-      id="lupa"
-      value="Buscar"
-      @click.prevent="buscarProdutos"
-    />
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos" />
   </form>
 </template>
 
