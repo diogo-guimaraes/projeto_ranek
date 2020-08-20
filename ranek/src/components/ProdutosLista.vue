21d9bb3e3ef4925bb5ab751f5f8f2ec02a8728cd
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-19 20:27:38 -0400

    refactor(compoments): ProdutosLista
    *  add computed-> url
    retrna os dados serializado na funlção  serialize em helpers retornando na rota  de busca o valor da url e adiciona paginete aponstado em produtosPorPagina
    
    *  add methods-> method->getProdutos
    padroniza axios como forma de requisição
    
    * add watch->url
    ativa o método getProdutos toda vez que a url é modificada

--- ranek/src/components/ProdutosLista.vue
+++ ranek/src/components/ProdutosLista.vue
@@ -6,11 +6,7 @@
   -->
   <section class="produtos-container">
     <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0].src"
-        :alt="produto.fotos[0].titulo"
-      />
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
       <p class="preco">{{ produto.preco }}</p>
       <h2 class="titulo">{{ produto.nome }}</h2>
       <p>{{ produto.descricao }}</p>
@@ -20,19 +16,37 @@
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 export default {
   data() {
     return {
       produtos: null,
+      produtosPorPagina: 9,
     };
   },
+
+  computed: {
+    // retrna os dados serializado na funlção  serialize em helpers retornando na rota  de busca o valor da url e adiciona paginete aponstado em produtosPorPagina
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    },
+  },
   methods: {
+    // faz um fech na api e apresenta os dados quando ativado a primeira vez
     getProdutos() {
-      api.get("/produto").then((response) => {
+      api.get(this.url).then((response) => {
         this.produtos = response.data;
       });
     },
   },
+  // ativa o método getProdutos toda vez que a url é modificada
+  watch: {
+    url() {
+      this.getProdutos();
+    },
+  },
+  // ativa o método getProdutos toda vez que a aplicação é iniciada
   created() {
     this.getProdutos();
   },
