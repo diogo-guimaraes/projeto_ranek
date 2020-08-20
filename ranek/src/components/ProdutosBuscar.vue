bc1b87235187320a8b422dd728d20ff4cc8293bb
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-19 20:28:07 -0400

    refactor(compoments): ProdutosLista
    * comentários

--- ranek/src/components/ProdutosBuscar.vue
+++ ranek/src/components/ProdutosBuscar.vue
@@ -5,6 +5,9 @@
     rm(compoments): ProdutosLista
   -->
   <form>
+    <!-- adiciona input para busca passando a diretiva v-model apontando para data()
+     e um botão passando @click que ao clicar ativará o método buscarProdutos
+    e prevente para prevenir que o browser atualize quando o botão for clicado-->
     <input name="busca" id="busca" type="text" v-model="busca" />
     <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos" />
   </form>
@@ -18,6 +21,9 @@ export default {
     };
   },
   methods: {
+    // criamos a função buscarProdutos que regastará o valor em data busca
+    //   para ser possível passar parametros para usar na url, passamos um objeto chamado query que é
+    //  um parâmetro de query
     buscarProdutos() {
       this.$router.push({ query: { q: this.busca } });
     },
