87fcdad44e4463dd8f5d27bc89fe88082ea666b9
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-19 21:46:22 -0400

    refactor(compoments): ProdutosLista
    *  add placeholder
    adciona texto inicial na busca
    
    * add style para busca

--- ranek/src/components/ProdutosBuscar.vue
+++ ranek/src/components/ProdutosBuscar.vue
@@ -8,7 +8,7 @@
     <!-- adiciona input para busca passando a diretiva v-model apontando para data()
      e um botão passando @click que ao clicar ativará o método buscarProdutos
     e prevente para prevenir que o browser atualize quando o botão for clicado-->
-    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input name="busca" id="busca" type="text" v-model="busca" placeholder="Buscar..." />
     <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos" />
   </form>
 </template>
@@ -31,4 +31,39 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.1);
+  padding: 20px;
+  border: none;
+  border-radius: 4px;
+  transition: all 0.3s;
+}
+
+#busca:focus,
+#busca:hover {
+  outline: none;
+  transform: scale(1.1);
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
+</style>
