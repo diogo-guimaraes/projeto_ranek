f0d3504e2f501788f65fe0324f620ea04f468231
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-19 21:03:22 -0400

    refactor(compoments): ProdutosLista
    * add class produtos
    verifica se tem o produto buscado
    
    * add router link envolvendo o produto pra quando ser clicado, levar para página do produto
    
    * add class produto
    para estilizar  os produtos apresentados pelo axios
    
    * add reposta caso o produto não seja encontrado utilizando v-else-if=

--- ranek/src/components/ProdutosLista.vue
+++ ranek/src/components/ProdutosLista.vue
@@ -5,11 +5,18 @@
     rm(compoments): ProdutosLista
   -->
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p>{{ produto.descricao }}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
