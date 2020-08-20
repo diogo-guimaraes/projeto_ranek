097b431a049febcc66cedd8dfff19321a9e4d5ed
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-20 19:59:47 -0400

    refactor(compoments): ProdutosPaginar
    * add compenente para paginar páginas dos produtos
    
    * add props para declarar as propriedades dos objetos

---
+++ ranek/src/components/ProdutosPaginar.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 
+    refactor(compoments): ProdutosPaginar
+    add(compoments): ProdutosPaginar
+    rm(compoments): ProdutosPaginar
+  -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    produtosPorPagina: {
+      type: Number,
+      default: 1,
+    },
+    produtosTotal: {
+      type: Number,
+      default: 1,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
