06faa33f46d641cf118e8c766a5c6877cd22d634
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-24 07:58:43 -0400

    refactor(compoments): ProdutosPaginar
    <ul v-if="paginasTotal > 1">
        <li v-for="pagina in paginasTotal" :key="pagina">
          <router-link :to="{query: query(pagina)}">{{pagina}}</router-link>
        </li>
      </ul>

--- ranek/src/components/ProdutosPaginar.vue
+++ ranek/src/components/ProdutosPaginar.vue
@@ -4,7 +4,11 @@
     add(compoments): ProdutosPaginar
     rm(compoments): ProdutosPaginar
   -->
-  <div></div>
+  <ul v-if="paginasTotal > 1">
+    <li v-for="pagina in paginasTotal" :key="pagina">
+      <router-link :to="{query: query(pagina)}">{{pagina}}</router-link>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -19,8 +23,41 @@ export default {
       default: 1,
     },
   },
+  methods: {
+    query(pagina) {
+      return {
+        ...this.$route.query,
+        _page: pagina,
+      };
+    },
+  },
+  computed: {
+    paginasTotal() {
+      const total = this.produtosTotal / this.produtosPorPagina;
+      return total !== Infinity ? Math.ceil(total) : 0;
+    },
+  },
 };
 </script>
 
 <style scoped>
+ul {
+  grid-column: 1 / -1;
+}
+
+li {
+  display: inline-block;
+}
+
+li a {
+  padding: 2px 8px;
+  border-radius: 2px;
+  margin: 4px;
+}
+
+li a.router-link-exact-active,
+li a:hover {
+  background: #87f;
+  color: #fff;
+}
 </style>
