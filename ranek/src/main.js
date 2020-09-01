aa3f4fd31fb358d6d10a04bb792445841501b322
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-31 22:59:37 -0400

    refactor: main
    *  add Vue.filter
    
    add filtro para converter integer para float

--- ranek/src/main.js
+++ ranek/src/main.js
@@ -11,6 +11,18 @@ import PaginaCarregando from "@/components/PaginaCarregando.vue";
 Vue.config.productionTip = false;
 
 Vue.component("PaginaCarregando", PaginaCarregando);
+// add filtro para converter integer para float
+Vue.filter("numeroPreco", valor => {
+  valor = Number(valor);
+  if (!isNaN(valor)) {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    });
+  } else {
+    return "";
+  }
+});
 
 new Vue({
   router,
