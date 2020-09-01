151b6e868a096bd877bdb9339753fda0ddc815d3
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-31 23:05:03 -0400

    refactor(compoments): LoginCriar
    * add página criar login

---
+++ ranek/src/components/LoginCriar.vue
@@ -0,0 +1,18 @@
+<template>
+<!-- 
+  refactor(compoments): LoginCriar
+  add(compoments): LoginCriar
+  rm(compoments): LoginCriar
+-->
+<div></div>
+
+</template>
+
+<script>
+export default {
+  name: "LoginCriar"
+};
+</script>
+
+<style>
+</style>
