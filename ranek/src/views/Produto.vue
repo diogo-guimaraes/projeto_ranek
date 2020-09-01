0f2777bd367da02ed2b3ef06026f70887cdc5879
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-31 21:34:29 -0400

    add(view): Produto
    *  add página produto com nome e no props id
    
    * add data retornando  produto iniciando null

---
+++ ranek/src/views/Produto.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+     name: "Produtos",
+  props: ["id"],
+  data() {
+    return {
+      produto: null
+    };
+  },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
