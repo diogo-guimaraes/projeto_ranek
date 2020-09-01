e01ca24d76ac24d85d06ed2fe04c6f0ea83fea34
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-31 20:50:49 -0400

    refactor(compoments): ProdutosLista
    *  add component  PaginaCarregando globalmente
    
    * import PaginaCarregando

--- ranek/src/main.js
+++ ranek/src/main.js
@@ -1,9 +1,16 @@
+
+// refactor(compoments): ProdutosLista
+// add(compoments): ProdutosLista
+// rm(compoments): ProdutosLista
+ 
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import PaginaCarregando from "@/components/PaginaCarregando.vue";
 Vue.config.productionTip = false;
+
+Vue.component("PaginaCarregando", PaginaCarregando);
 
 new Vue({
   router,
