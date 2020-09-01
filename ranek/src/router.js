d067bebeff15672dcc1b7d6cec50350476593955
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-08-31 20:56:33 -0400

    refactor:router
    * add scrollBehavior
    
    função para subir a tela de forma suave

--- ranek/src/router.js
+++ ranek/src/router.js
@@ -1,3 +1,6 @@
+// refactor:router
+// add:router
+// rm:router
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
@@ -12,6 +15,10 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home,
-    },
+    },   
   ],
+  // função para subir a tela de forma suave 
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 });
