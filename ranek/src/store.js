6d4fb87f1177eb9e2ec9a330708c7c8048dd18c8
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-01 21:18:46 -0400

    refactor(src): Store
    * import api
    
    * add atributos a serem usados no cadastro e login

--- ranek/src/store.js
+++ ranek/src/store.js
@@ -1,10 +1,28 @@
+// refactor(src): Store
+// add(src): Store
+// rm(src): Store 
+
 import Vue from "vue";
 import Vuex from "vuex";
+import { api } from "@/services.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    login: false,
+    usuario: {
+      id: "",
+      nome: "",
+      email: "",
+      senha: "",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: ""
+    }
+  },
+  
 });
