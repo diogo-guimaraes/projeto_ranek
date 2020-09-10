53c9121de4dd099b9c073266bd9e5a4ce0ad260c
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-09 21:26:54 -0400

    add(compoments): Usuario
    add página do usuário

---
+++ ranek/src/views/usuario/Usuario.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- 
+    refactor(compoments): Usuario
+    add(compoments): Usuario
+    rm(compoments): Usuario
+  -->
+  <p>Página do Usuário: {{$store.state.usuario}}</p>
+</template>
+
+<script>
+export default {
+  name: "Usuario"
+};
+</script>
+
+<style>
+</style>
