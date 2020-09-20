fac2284fc805d997180356714a1dfaecc8a5657e
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-20 18:17:44 -0400

    add(component): ProdutoAdicionar
    * add component para adicionar produto ao carrinho

---
+++ ranek/src/components/ProdutoAdicionar.vue
@@ -0,0 +1,17 @@
+<template>
+<!-- 
+    refactor(component): ProdutoAdicionar
+    add(component): ProdutoAdicionar
+    rm(component): ProdutoAdicionar
+  -->
+  <p>Produto Adicionar</p>
+</template>
+
+<script>
+export default {
+  name: "ProdutoAdicionar"
+};
+</script>
+
+<style>
+</style>
