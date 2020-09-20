55a44ed492b66ab3002114d1196573219d0e5b74
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-20 14:55:53 -0400

    add(view): UsuarioCompras
    * add nova view ao projeto
    
    * será usado na view usuário

---
+++ ranek/src/views/usuario/UsuarioCompras.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 
+    refactor(view): UsuarioCompras
+    add(view): UsuarioCompras
+    rm(view): UsuarioCompras
+  -->
+  <p>Lista de Compras</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
