f4ee039aafc14fb2da3238f3a5060d8ff650b029
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-20 14:54:43 -0400

    add(view): UsuarioVendas
    * add nova view ao projeto

---
+++ ranek/src/views/usuario/UsuarioVendas.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 
+    refactor(view): UsuarioVendas
+    add(view): UsuarioVendas
+    rm(view): UsuarioVendas
+  -->
+  <p>Lista de Vendas</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
