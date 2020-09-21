7b716678923adec3b7f851a0b60da0608d9ab690
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-20 20:59:18 -0400

    refactor(view): UsuarioProdutos
    * importa mapState para mapear os estados,
    * add computed, mapState
    
     * Passando login para verificar se o usuário está logado , usuario para puxas as informações do usuário e  produtos também

--- ranek/src/views/usuario/UsuarioProdutos.vue
+++ ranek/src/views/usuario/UsuarioProdutos.vue
@@ -8,17 +8,33 @@
     <h2>Adicionar Produto</h2>
     <ProdutoAdicionar/>
     <h2>Seus Produtos</h2>
-    
+    {{usuario_produtos}}
   </section>
 </template>
 
 <script>
 import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import { mapState, mapActions } from "vuex";
 export default {
    name: "UsuarioProdutos",
    components: {
-    ProdutoAdicionar,   
+    ProdutoAdicionar,
   },
+  //  Passando login para verificar se o usuário está logado , usuario para puxas as informações do usuário e  produtos também
+  computed: {
+    // mapeia os estados das priedades 
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  // mapeia e puxa ação de getUsuarioProdutos
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  created() {
+    if (this.login) {
+      // se logir for true faz um getUsuarioProdutos
+      this.getUsuarioProdutos();
+    }
+  }  
 };
 </script>
 
