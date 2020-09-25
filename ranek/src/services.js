1a4bdfacf9c0963a02e51d3a553e848ba41f5b72
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-25 13:09:26 -0300

    refactor(src): services
    * add put
    
    para atualizar

--- ranek/src/services.js
+++ ranek/src/services.js
@@ -16,6 +16,9 @@ export const api = {
   post(endpoint, body) {
     return axiosInstance.post(endpoint, body);
   },
+  put(endpoint, body) {
+    return axiosInstance.put(endpoint, body);
+  },
   delete(endpoint) {
     return axiosInstance.delete(endpoint);
   }
