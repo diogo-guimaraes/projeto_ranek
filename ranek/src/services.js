4ea0e2c53662f4ff5307d3135bd4386ab2cfefd6
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-26 08:58:18 -0300

    refactor(src): services
    * aponta endpoint api wp
    
    * addaxiosInstance.interceptors para interceptar o e guardar o token no localStorage
    
    * add login
    
    axios.post na rota pra login acompanhado do body que é  onde vai o username e o password

--- ranek/src/services.js
+++ ranek/src/services.js
@@ -4,11 +4,23 @@
 
 // aqui basicamente ficam os servicos externos
 import axios from "axios";
-
+// http://localhost:3000 -> ranek-api.json
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost/wp_ranek/wp-json/api",
 });
-
+// interceptador
+axiosInstance.interceptors.request.use(
+  function(config){
+    const token = window.localStorage.token;
+    if(token){
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  function (error){
+    return Promise.reject(error);
+  }
+)
 export const api = {
   get(endpoint) {
     return axiosInstance.get(endpoint);
@@ -21,7 +33,14 @@ export const api = {
   },
   delete(endpoint) {
     return axiosInstance.delete(endpoint);
+  },
+  login(body){
+    return axios.post("http://localhost/wp_ranek/wp-json/jwt-auth/v1/token", body);
+  },
+  validateToken(){
+    return axios.post("http://localhost/wp_ranek/wp-json/jwt-auth/v1/token/validate");
   }
+  
 };
 
 // faz axios na api via cep
