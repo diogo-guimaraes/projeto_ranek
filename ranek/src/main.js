7708f02b9e56fcc0a6f06f45ef610c96cd970529
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-28 11:12:14 -0300

    refactor(src): main
    * registra globlamente componente para tratar erros

--- ranek/src/main.js
+++ ranek/src/main.js
@@ -1,16 +1,18 @@
 
-// refactor: main
-// add: main
-// rm: main
+// refactor(src): main
+// add(src): main
+// rm(src): main
  
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import PaginaCarregando from "@/components/PaginaCarregando.vue";
+import ErroNotificacao from "@/components/ErroNotificacao.vue";
 Vue.config.productionTip = false;
 
 Vue.component("PaginaCarregando", PaginaCarregando);
+Vue.component("ErroNotificacao", ErroNotificacao);
 // add filtro para converter integer para float
 Vue.filter("numeroPreco", valor => {
   valor = Number(valor);
