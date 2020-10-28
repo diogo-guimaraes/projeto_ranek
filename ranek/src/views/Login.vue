a8b863c893d279346e3c12806213834f850db585
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-28 11:15:14 -0300

    refactor(view): Login
    * add estrutura de tratamento para erro

--- ranek/src/views/Login.vue
+++ ranek/src/views/Login.vue
@@ -12,9 +12,10 @@
       <label for="senha">Senha</label>
       <input type="password" name="senha" id="senha" v-model="login.senha">
       <button class="btn" @click.prevent="logar">Logar</button>
+      <ErroNotificacao :erros = "erros"/>
     </form>
     <p class="perdeu">
-      <a href="/" target="_blank">Perdeu a senha? Clique aqui.</a>
+      <a href="http://localhost/wp_ranek/wp-login.php?action=lostpassword" target="_blank">Perdeu a senha? Clique aqui.</a>
     </p>
     <LoginCriar/>
   </section>
@@ -34,14 +35,19 @@ import LoginCriar from "@/components/LoginCriar.vue";
       login: {
         email: "",
         senha: ""
-      }
+      },
+      erros: []
     };
   },
   methods: {
     logar() {
+      this.erros = [];
       this.$store.dispatch("logarUsuario", this.login).then(response => {
         this.$store.dispatch("getUsuario");
         this.$router.push({ name: "usuario" });
+      })
+      .catch(error => {
+        this.erros.push(error.response.data.message);
       });
     }
   }
