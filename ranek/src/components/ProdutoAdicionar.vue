3f28be7090dab745d07d0cc06742ddb10aa57635
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-28 14:46:43 -0300

    refactor(component): ProdutoAdicionar
    * remove console.log(files)
    
    add async em adicionarProduto
    
    * add event em adicionarProduto
    
    para ter acesso e manipular as propriedades do butão

--- ranek/src/components/ProdutoAdicionar.vue
+++ ranek/src/components/ProdutoAdicionar.vue
@@ -40,7 +40,6 @@ export default {
       const form = new FormData();
 
       const files = this.$refs.fotos.files;
-      console.log(files);
       for (let i = 0; i < files.length; i++) {
         form.append(files[i].name, files[i]);
       }
@@ -53,11 +52,18 @@ export default {
 
       return form;
     },
-    adicionarProduto() {
+    async adicionarProduto(event) {
       const produto = this.formatarProduto();
-      api.post("/produto", produto).then(() => {
-        this.$store.dispatch("getUsuarioProdutos");
-      });
+
+      const button = event.currentTarget;
+      button.value = "Adicionando...";
+      button.setAttribute("disabled", "");
+
+      await api.post("/produto", produto);
+      await this.$store.dispatch("getUsuarioProdutos");
+
+      button.removeAttribute("disabled");
+      button.value = "Adicionar Produto";
     }
   }
 };
