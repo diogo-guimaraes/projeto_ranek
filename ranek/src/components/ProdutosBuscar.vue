b78c2c6f79ea1d8e689581367197c8f12261149d
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-28 11:51:12 -0300

    refactor(compoments): ProdutosLista
    * add ajustes para tornar a páina mais responsiva

--- ranek/src/components/ProdutosBuscar.vue
+++ ranek/src/components/ProdutosBuscar.vue
@@ -36,22 +36,18 @@ form {
   max-width: 600px;
   margin: 30px auto 60px auto;
   position: relative;
+  padding: 0 10px;
 }
 
 #busca {
   width: 100%;
-  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.1);
   padding: 20px;
   border: none;
-  border-radius: 4px;
-  transition: all 0.3s;
 }
 
 #busca:focus,
 #busca:hover {
-  outline: none;
   transform: scale(1.1);
-  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
 }
 
 #lupa {
