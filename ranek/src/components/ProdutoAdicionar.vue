81cf695c7dc44befa249bd49996fe93b32476095
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-28 09:44:07 -0300

    refactor(component): ProdutoAdicionar
    * add prepara estrutura para upload de produtos com de fotos

--- ranek/src/components/ProdutoAdicionar.vue
+++ ranek/src/components/ProdutoAdicionar.vue
@@ -37,7 +37,21 @@ export default {
   methods: {
     // formata prduto antes de enviar para a api
     formatarProduto() {
-      this.produto.usuario_id = this.$store.state.usuario.id;
+      const form = new FormData();
+
+      const files = this.$refs.fotos.files;
+      console.log(files);
+      for (let i = 0; i < files.length; i++) {
+        form.append(files[i].name, files[i]);
+      }
+
+      form.append("nome", this.produto.nome);
+      form.append("preco", this.produto.preco);
+      form.append("descricao", this.produto.descricao);
+      form.append("vendido", this.produto.vendido);
+      form.append("usuario_id", this.$store.state.usuario.id);
+
+      return form;
     },
     adicionarProduto() {
       this.formatarProduto();
