e0e94812671bfd20c2f43f93d32a932d8260c4ea
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-28 14:14:28 -0300

    refactor(src): services
    * add cometários com exemplo de url no modo produção

--- ranek/src/services.js
+++ ranek/src/services.js
@@ -2,10 +2,15 @@
 // add(src): services
 // rm(src): services
 
+// para modo produção
+// const url = "https://ranekapi.origamid.dev/wp-json";
+
 // aqui basicamente ficam os servicos externos
 import axios from "axios";
 // http://localhost:3000 -> ranek-api.json
 const axiosInstance = axios.create({
+  // para modo produção
+  // baseURL: url + "/api"
   baseURL: "http://localhost/wp_ranek/wp-json/api",
 });
 // interceptador
@@ -35,9 +40,13 @@ export const api = {
     return axiosInstance.delete(endpoint);
   },
   login(body){
+    // para modo produção
+    // return axios.post(url + "/jwt-auth/v1/token", body);
     return axios.post("http://localhost/wp_ranek/wp-json/jwt-auth/v1/token", body);
   },
   validateToken(){
+    // para modo produção
+    // return axiosInstance.post(url + "/jwt-auth/v1/token/validate");
     return axiosInstance.post("http://localhost/wp_ranek/wp-json/jwt-auth/v1/token/validate");
   }
   
